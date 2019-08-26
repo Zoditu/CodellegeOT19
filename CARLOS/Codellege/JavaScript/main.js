0c6f8fabed11d3a8470f945dc084825127b656ca
Carlos Mendoza <charles.mendoza.and97@gmail.com>
2019-08-26 14:05:26 -0500

    JavaScript

---
+++ CARLOS/Codellege/JavaScript/main.js
@@ -0,0 +1,17 @@
+var Nombre="Carlos";
+var Apellidos="Mendoza Andrade";
+var Edad= 22;
+
+console.log(Nombre);
+console.log(Edad);
+
+Edad=21;
+console.log(Edad);
+
+function Prepararquesadilla(Tortilla, queso){
+console.log("Poner queso del tipo:"+ queso+ "en tortilla");
+console.log("Poner en el comal");
+console.log("Listo");
+}
+
+Prepararquesadilla();
