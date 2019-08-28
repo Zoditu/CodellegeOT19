e2b6d0a4cff1f59c73744ae86ffccfd0df37e9ae
Joaquin Reyes Espinoza <jreyes3344@ppsc.edu.mxclear>
2019-08-28 13:56:00 -0500

    update 28/08/2019

---
+++ Aaron - Codellege/HTML/styles/cumpleaños.js
@@ -0,0 +1,39 @@
+var alumnos = [
+    {name: "Aaron Arredondo", birthday: "23 de Enero"},
+    {name: "Natalia Capitán", birthday: "7 de Junio"},
+    {name: "Karla GH", birthday: "3 de Noviembre"},
+    {name: "Aldo Matias", birthday: "19 de Octubre"},
+    {name: "Ricardo Teneyuque", birthday: "17 de Octubre"},
+    {name: "Javier Anguiano", birthday: "22 de Mayo"},
+    {name: "Andrés Coronado", birthday: "18 de Noviembre"},
+    {name: "Miriam Herrera", birthday: "9 de Noviembre"},
+    {name: "Celestino Gónzalez", birthday: "9 de Marzo"},
+    {name: "Carlos Mendoza", birthday: "25 de Abril"},
+    {name: "Liliana Hernández", birthday: "16 de Septiembre"}
+];
+
+window.onload = function() {
+    var main = document.getElementById("main");
+    var section = document;
+
+    for (var i=0; i<alumnos.length; i++) {
+        main.innerHTML +=
+        '<article class="maestros">' +
+        '<section class="tarjeta">' +
+            '<div class="imagen">' +
+                '<img src="https://hackernoon.com/hn-images/1*zm5NLjdhGd3VVTA2u-xEPg.gif" alt="">' +
+            '</div>' +
+            '<div class="datos">' +
+                '<header>' +
+                    '<h3 class="centrado">' + alumnos[i].name +'</h3></header>' +
+                '<main>' +
+                    '<h5 class="derecha">' + alumnos[i].birthday + '</h5></main>' +
+                '<footer>' +
+                    '<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias numquam fuga nisi ut voluptate nesciunt ex alias recusandae quae! Assumenda aperiam vitae repudiandae quia obcaecati. Natus tempore assumenda optio quo? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam incidunt consectetur debitis asperiores aliquam veritatis quia ab architecto voluptates, qui quam maiores dolorum necessitatibus assumenda accusamus corporis neque accusantium rerum.</p>' +
+                '</footer>' +
+            '</div>' +
+            '<div class="clear"></div>' +
+        '</section>' +
+    '</article>';
+    }
+}
---
+++ Aaron - Codellege/HTML/styles/main.js
@@ -0,0 +1,47 @@
+var alumnos = [
+    "Aaron Arredondo",
+    "Karla GH",
+    "Javier Anguiano",
+    "Ricardo Teneyuque",
+    "Aldo Matias",
+    "Andrés Coronado",
+    "Andrés Ramos",
+    "Itzel Alonso",
+    "Miriam Herrera",
+    "Carlos Mendoza",
+    "Celestino Gozales"
+];
+
+
+window.onload = function () {
+    var main = document.getElementById("body");
+    var section = document;
+
+    for (var i=0; i<alumnos.length; i++) {
+        main.innerHTML +=
+        '<article class="gafete">' +
+        '<header>' +
+            '<section id="agujero">' +
+                    '<div id="agujero1"></div>' +
+            '</section>' +
+            '<section id="logo">' +
+                    '<div id="logo1"><img src="http://www.softtek.com/stkignite/img/logo-soft.png" alt=""></div>' +
+            '</section>' +
+        '</header>' +
+        '<main>' +
+            '<section id="foto">' +
+                '<div id="foto1"><img src="img/perfil.png" alt=""></div>' +
+            '</section>' +
+            '<section id="nombre">' +
+                '<div id="nombre1">' + alumnos[i] + '</div>' +
+            '</section>' +
+        '</main>' +
+        '<footer>' +
+            '<section id="logoC">' +
+                '<div id="logoC1"><img src="img/logo.svg" alt=""></div>' +
+            '</section>'+
+        '</footer>' +
+
+    '</article>';
+    }
+}
