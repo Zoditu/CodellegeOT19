71a549dafb3c88cfd743b1be022f9debddb88841
Carlos Mendoza <charles.mendoza.and97@gmail.com>
2019-08-28 00:10:00 -0500

    GaffetesJS

---
+++ CARLOS/Codellege/JavaScript/JS/Nombreall.js
@@ -0,0 +1,194 @@
+var alumnos=[
+    {
+   //  Carlos
+        Nombre:"Carlos",
+        ApellidoPaterno:"Mendoza",
+        ApellidoMaterno:"Andrade",
+        Edad:22,
+        Estatura: 1.80,
+        Sexo: "H",
+        Nacionalidad: "Mexicano",
+        mostrar: function(){
+            console.log(this.Nombre+" "+this.ApellidoPaterno);
+            console.log("Edad: "+ this.Edad);
+        }
+    },
+   // Itzel
+    {
+       Nombre:"Itzel",
+       ApellidoPaterno:"Alonso ",
+       ApellidoMaterno:"Garcia",
+       Edad:22,
+       Estatura: 1.72,
+       Sexo: "M",
+       Nacionalidad: "Mexicano",
+       mostrar: function(){
+           console.log(this.Nombre+" "+this.ApellidoPaterno);
+           console.log("Edad: "+ this.Edad);
+       }
+   },
+
+   {
+       Nombre:"Liliana",
+       ApellidoPaterno:"Hernadnez",
+       ApellidoMaterno:"Gonzales",
+       Edad:19,
+       Estatura: 1.70,
+       Sexo: "M",
+       Nacionalidad: "Mexicano",
+       mostrar: function(){
+           console.log(this.Nombre+" "+this.ApellidoPaterno);
+           console.log("Edad: "+ this.Edad);
+       }
+   },
+
+   {
+       Nombre:"Aldo",
+       ApellidoPaterno:"MAtias",
+       ApellidoMaterno:"Galindo",
+       Edad:23,
+       Estatura: 1.77,
+       Sexo: "H",
+       Nacionalidad: "Mexicano",
+       mostrar: function(){
+           console.log(this.Nombre+" "+this.ApellidoPaterno);
+           console.log("Edad: "+ this.Edad);
+       }
+   },
+
+   {
+       Nombre:"Aaron",
+       ApellidoPaterno:"Arredondo",
+       ApellidoMaterno:"Sarauz",
+       Edad:20,
+       Estatura: 1.74,
+       Sexo: "H",
+       Nacionalidad: "Mexicano",
+       mostrar: function(){
+           console.log(this.Nombre+" "+this.ApellidoPaterno);
+           console.log("Edad: "+ this.Edad);
+       }
+   },
+//  Karla
+   {
+       Nombre:"Karla",
+       ApellidoPaterno:"Gonzalez",
+       ApellidoMaterno:"Hernandez",
+       Edad:20,
+       Estatura: 1.48,
+       Sexo: "M",
+       Nacionalidad: "Mexicano",
+       mostrar: function(){
+       console.log(this.Nombre+" "+this.ApellidoPaterno);
+       console.log("Edad: "+ this.Edad);
+       }
+   },
+//  Natalia
+   {
+       Nombre:"Natalia",
+       ApellidoPaterno:"Rodriguez",
+       ApellidoMaterno:"Capitan",
+       Edad:22,
+       Estatura: 1.56,
+       Sexo: "M",
+       Nacionalidad: "Mexicano",
+       mostrar: function(){
+           console.log(this.Nombre+" "+this.ApellidoPaterno);
+           console.log("Edad: "+ this.Edad);
+       }
+   },
+//  Andress
+   {
+       Nombre:"Andres",
+       ApellidoPaterno:"Ramos",
+       ApellidoMaterno:"Escobar",
+       Edad:19,
+       Estatura: 1.60,
+       Sexo: "H",
+       Nacionalidad: "Colombiano",
+       mostrar: function(){
+           console.log(this.Nombre+" "+this.ApellidoPaterno);
+           console.log("Edad: "+ this.Edad);
+       }
+   },
+
+   {
+       Nombre:"Ricardo",
+       ApellidoPaterno:"Teneyuque",
+       ApellidoMaterno:"Guerta",
+       Edad:20,
+       Estatura: 1.70,
+       Sexo: "H",
+       Nacionalidad: "Mexicano",
+       mostrar: function(){
+           console.log(this.Nombre+" "+this.ApellidoPaterno);
+           console.log("Edad: "+ this.Edad);
+       }
+   },
+
+   {
+       Nombre:"Javier",
+       ApellidoPaterno:"Anguiano",
+       ApellidoMaterno:"de lara",
+       Edad:21,
+       Estatura: 1.77,
+       Sexo: "H",
+       Nacionalidad: "Mexicano",
+       mostrar: function(){
+           console.log(this.Nombre+" "+this.ApellidoPaterno);
+           console.log("Edad: "+ this.Edad);
+       }
+   },
+
+   {
+       Nombre:"Andres",
+       ApellidoPaterno:"Coronado",
+       ApellidoMaterno:"Lazaro",
+       Edad:22,
+       Estatura: 1.75,
+       Sexo: "H",
+       Nacionalidad: "Mexicano",
+       mostrar: function(){
+           console.log(this.Nombre+" "+this.ApellidoPaterno);
+           console.log("Edad: "+ this.Edad);
+       }
+   },
+
+   {
+       Nombre:"Miriam",
+       ApellidoPaterno:"Herrera",
+       ApellidoMaterno:"Reyna",
+       Edad:21,
+       Estatura: 1.60,
+       Sexo: "M",
+       Nacionalidad: "Mexicano",
+       mostrar: function(){
+           console.log(this.Nombre+" "+this.ApellidoPaterno);
+           console.log("Edad: "+ this.Edad);
+       }
+   },
+
+   {
+       Nombre:"Celestino",
+       ApellidoPaterno:"Gonzales",
+       ApellidoMaterno:"Marquez",
+       Edad:20,
+       Estatura: 1.60,
+       Sexo: "H",
+       Nacionalidad: "Mexicano",
+       mostrar: function(){
+           console.log(this.Nombre+" "+this.ApellidoPaterno);
+           console.log("Edad: "+ this.Edad);
+       }
+   },
+
+]
+
+
+window.onload=function(){
+  var main=document.getElementById("main");
+  for(var i=0; i<alumnos.length; i++){
+    main.innerHTML+= "<h4>"+ alumnos[i].Nombre + " "+ alumnos[i].ApellidoPaterno + " "+ alumnos[i].ApellidoMaterno + "</h4>";
+    
+  }
+}
