549a1a00a99891f3f378673102df678925038909
Carlos Mendoza <charles.mendoza.and97@gmail.com>
2019-09-03 11:30:39 -0500

    subida

---
+++ CARLOS/Codellege/JavaScript/JS/GafeteJQ.js
@@ -0,0 +1,279 @@
+var alumnos=[
+
+    {
+   //  Carlos
+        Nombre:"Carlos",
+        ApellidoPaterno:"Mendoza",
+        ApellidoMaterno:"Andrade",
+        Edad:22,
+        Estatura: 1.80,
+        Sexo: "H",
+        Nacionalidad: "Mexicano",
+        foto: "Imagenes/fotogafete.jpeg",
+        NombreCompleto: function(){
+            return this.Nombre + " "+ this.ApellidoPaterno+" "+ this.ApellidoMaterno;
+        },
+        mostrar: function(){
+            console.log(this.Nombre+" "+this.ApellidoPaterno);
+            console.log("Edad: "+ this.Edad);
+        }
+        
+    },
+   // Itzel
+    {
+       Nombre:"Itzel",
+       ApellidoPaterno:"Alonso ",
+       ApellidoMaterno:"Garcia",
+       Edad:22,
+       Estatura: 1.72,
+       Sexo: "M",
+       Nacionalidad: "Mexicano",
+       foto: "Imagenes/fotogafete.jpeg",
+       NombreCompleto: function(){
+        return this.Nombre + " "+ this.ApellidoPaterno+" "+ this.ApellidoMaterno;
+    },
+       mostrar: function(){
+           console.log(this.Nombre+" "+this.ApellidoPaterno);
+           console.log("Edad: "+ this.Edad);
+       }
+   },
+
+   {
+       Nombre:"Liliana",
+       ApellidoPaterno:"Hernadnez",
+       ApellidoMaterno:"Gonzales",
+       Edad:19,
+       Estatura: 1.70,
+       Sexo: "M",
+       Nacionalidad: "Mexicano",
+       foto: "Imagenes/liliana.jpg",
+       NombreCompleto: function(){
+        return this.Nombre + " "+ this.ApellidoPaterno+" "+ this.ApellidoMaterno;
+    },
+       mostrar: function(){
+           console.log(this.Nombre+" "+this.ApellidoPaterno);
+           console.log("Edad: "+ this.Edad);
+       }
+   },
+
+   {
+       Nombre:"Aldo",
+       ApellidoPaterno:"MAtias",
+       ApellidoMaterno:"Galindo",
+       Edad:23,
+       Estatura: 1.77,
+       Sexo: "H",
+       Nacionalidad: "Mexicano",
+       foto: "Imagenes/fotogafete.jpeg",
+       NombreCompleto: function(){
+        return this.Nombre + " "+ this.ApellidoPaterno+" "+ this.ApellidoMaterno;
+    },
+       mostrar: function(){
+           console.log(this.Nombre+" "+this.ApellidoPaterno);
+           console.log("Edad: "+ this.Edad);
+       }
+   },
+
+   {
+       Nombre:"Aaron",
+       ApellidoPaterno:"Arredondo",
+       ApellidoMaterno:"Sarauz",
+       Edad:20,
+       Estatura: 1.74,
+       Sexo: "H",
+       Nacionalidad: "Mexicano",
+       foto: "Imagenes/Aaron.jpg",
+       NombreCompleto: function(){
+        return this.Nombre + " "+ this.ApellidoPaterno+" "+ this.ApellidoMaterno;
+    },
+       mostrar: function(){
+           console.log(this.Nombre+" "+this.ApellidoPaterno);
+           console.log("Edad: "+ this.Edad);
+       }
+   },
+//  Karla
+   {
+       Nombre:"Karla",
+       ApellidoPaterno:"Gonzalez",
+       ApellidoMaterno:"Hernandez",
+       Edad:20,
+       Estatura: 1.48,
+       Sexo: "M",
+       Nacionalidad: "Mexicano",
+       foto: "Imagenes/Karla.jpg",
+       NombreCompleto: function(){
+        return this.Nombre + " "+ this.ApellidoPaterno+" "+ this.ApellidoMaterno;
+    },
+       mostrar: function(){
+       console.log(this.Nombre+" "+this.ApellidoPaterno);
+       console.log("Edad: "+ this.Edad);
+       }
+   },
+//  Natalia
+   {
+       Nombre:"Natalia",
+       ApellidoPaterno:"Rodriguez",
+       ApellidoMaterno:"Capitan",
+       Edad:22,
+       Estatura: 1.56,
+       Sexo: "M",
+       Nacionalidad: "Mexicano",
+       foto: "Imagenes/fotogafete.jpeg",
+       NombreCompleto: function(){
+        return this.Nombre + " "+ this.ApellidoPaterno+" "+ this.ApellidoMaterno;
+    },
+       mostrar: function(){
+           console.log(this.Nombre+" "+this.ApellidoPaterno);
+           console.log("Edad: "+ this.Edad);
+       }
+   },
+//  Andress
+   {
+       Nombre:"Andres",
+       ApellidoPaterno:"Ramos",
+       ApellidoMaterno:"Escobar",
+       Edad:19,
+       Estatura: 1.60,
+       Sexo: "H",
+       Nacionalidad: "Colombiano",
+       foto: "Imagenes/fotogafete.jpeg",
+       NombreCompleto: function(){
+        return this.Nombre + " "+ this.ApellidoPaterno+" "+ this.ApellidoMaterno;
+    },
+       mostrar: function(){
+           console.log(this.Nombre+" "+this.ApellidoPaterno);
+           console.log("Edad: "+ this.Edad);
+       }
+   },
+
+   {
+       Nombre:"Ricardo",
+       ApellidoPaterno:"Teneyuque",
+       ApellidoMaterno:"Guerta",
+       Edad:20,
+       Estatura: 1.70,
+       Sexo: "H",
+       Nacionalidad: "Mexicano",
+       foto: "Imagenes/fotogafete.jpeg",
+       NombreCompleto: function(){
+        return this.Nombre + " "+ this.ApellidoPaterno+" "+ this.ApellidoMaterno;
+    },
+       mostrar: function(){
+           console.log(this.Nombre+" "+this.ApellidoPaterno);
+           console.log("Edad: "+ this.Edad);
+       }
+   },
+
+   {
+       Nombre:"Javier",
+       ApellidoPaterno:"Anguiano",
+       ApellidoMaterno:"de lara",
+       Edad:21,
+       Estatura: 1.77,
+       Sexo: "H",
+       Nacionalidad: "Mexicano",
+       foto: "Imagenes/Javier.jpg",
+       NombreCompleto: function(){
+        return this.Nombre + " "+ this.ApellidoPaterno+" "+ this.ApellidoMaterno;
+    },
+       mostrar: function(){
+           console.log(this.Nombre+" "+this.ApellidoPaterno);
+           console.log("Edad: "+ this.Edad);
+       }
+   },
+
+   {
+       Nombre:"Andres",
+       ApellidoPaterno:"Coronado",
+       ApellidoMaterno:"Lazaro",
+       Edad:22,
+       Estatura: 1.75,
+       Sexo: "H",
+       Nacionalidad: "Mexicano",
+       foto: "Imagenes/fotogafete.jpeg",
+       NombreCompleto: function(){
+        return this.Nombre + " "+ this.ApellidoPaterno+" "+ this.ApellidoMaterno;
+    },
+       mostrar: function(){
+           console.log(this.Nombre+" "+this.ApellidoPaterno);
+           console.log("Edad: "+ this.Edad);
+       }
+   },
+
+   {
+       Nombre:"Miriam",
+       ApellidoPaterno:"Herrera",
+       ApellidoMaterno:"Reyna",
+       Edad:21,
+       Estatura: 1.60,
+       Sexo: "M",
+       Nacionalidad: "Mexicano",
+       foto: "Imagenes/miriam.jpg",
+       NombreCompleto: function(){
+        return this.Nombre + " "+ this.ApellidoPaterno+" "+ this.ApellidoMaterno;
+    },
+       mostrar: function(){
+           console.log(this.Nombre+" "+this.ApellidoPaterno);
+           console.log("Edad: "+ this.Edad);
+       }
+   },
+
+   {
+       Nombre:"Celestino",
+       ApellidoPaterno:"Gonzales",
+       ApellidoMaterno:"Marquez",
+       Edad:20,
+       Estatura: 1.60,
+       Sexo: "H",
+       Nacionalidad: "Mexicano",
+       foto: "Imagenes/fotogafete.jpeg",
+       NombreCompleto: function(){
+        return this.Nombre + " "+ this.ApellidoPaterno+" "+ this.ApellidoMaterno;
+    },
+       mostrar: function(){
+           console.log(this.Nombre+" "+this.ApellidoPaterno);
+           console.log("Edad: "+ this.Edad);
+       }
+   },
+
+]
+// window.onload=function(){
+//     var main= document.getElementById("main");
+//     for(var i=0; i<alumnos.length; i++){
+//         main.innerHTML+= 
+            
+//            '<article class="gafette">' +
+//            ' <section class="logosofttek">'+
+//                     '<div class="hueco"></div>' +
+//                     '<div class="clear"></div>'+
+//                     '<img src="Imagenes/Softtek.jpeg">'+
+//                 '</section>'+
+//                 '<section class="foto">'+
+//                     '<img src="'+ alumnos[i].foto+'"/>' +
+//                     '<div class="clear"></div>'+
+//                     '<div class="Nombre">'+alumnos[i].NombreCompleto()+'</div>'+
+//                '</section>'+
+//                 '<section class="logocode">'+
+//                     '<img src="Imagenes/logocodellege.jpeg"/>'+
+//                 '</section>'+
+//             '</article>'
+//     }
+// }
+$(function(){
+$("#main").html(
+'<article class="gafette">' +
+' <section class="logosofttek">'+
+         '<div class="hueco"></div>' +
+         '<div class="clear"></div>'+
+         '<img src="Imagenes/Softtek.jpeg">'+
+     '</section>'+
+     '<section class="foto">'+
+         '<img src="'+ alumnos[i].foto+'"/>' +
+         '<div class="clear"></div>'+
+         '<div class="Nombre">'+alumnos[i].NombreCompleto()+'</div>'+
+    '</section>'+
+     '<section class="logocode">'+
+         '<img src="Imagenes/logocodellege.jpeg"/>'+
+     '</section>'+
+ '</article>')
+});
