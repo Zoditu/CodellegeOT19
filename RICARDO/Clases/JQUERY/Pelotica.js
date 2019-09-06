7f64aa46cbad733fed11dc83077bc103f7d98d2a
RICHARD-0011 <ricardo.t.pbf@gmail.com>
2019-09-06 13:16:29 -0500

    up

---
+++ RICARDO/Clases/JQUERY/Pelotica.js
@@ -0,0 +1,15 @@
+var min_X = 0;
+var min_Y = 0;
+var max_X = innerWidth;
+var max_Y = innerHeight;
+
+$( function(){
+
+    var ball = $("#ball");
+    setInterval( function(){
+
+    }, 1000);
+
+});
+
+
