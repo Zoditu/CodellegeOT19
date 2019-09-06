816c9f860976b8fe6fa8aba7485d3b978fce96be
Joaquin Reyes Espinoza <jreyes3344@ppsc.edu.mxclear>
2019-09-06 13:18:22 -0500

    bouncing ball

---
+++ Aaron - Codellege/HTML/styles/pelota.js
@@ -0,0 +1,9 @@
+var maxX = window.innerWidth;
+var maxY = window.innerHeight;
+var minX = 0;
+var minY = 0;
+
+
+setInterval(function(){
+    $(".pelota").css( { bottom: "1rem", right: "1rem" } );
+}, 500);
