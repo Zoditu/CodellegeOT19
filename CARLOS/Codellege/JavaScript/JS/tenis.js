0a9ec8b5a8509aabea0cfd1d030b8776957dc861
Carlos Mendoza <charles.mendoza.and97@gmail.com>
2019-09-10 09:46:32 -0500

    up

---
+++ CARLOS/Codellege/JavaScript/JS/tenis.js
@@ -0,0 +1,12 @@
+const ArrowDer= 39;
+const ArrowIzq= 37;
+
+$(function()
+{
+var barra = $("#barra");
+barra.keydown(function(e){
+if(ArrowDer==e.KeyCode){
+barrra.css("riigth","50px");
+}
+});
+});
