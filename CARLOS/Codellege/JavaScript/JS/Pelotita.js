45a6a9c0daa3017af579987f4c74ec062f8a88d0
Carlos Mendoza <charles.mendoza.and97@gmail.com>
2019-09-10 10:53:04 -0500

    Revert "up"
    This reverts commit 0a9ec8b5a8509aabea0cfd1d030b8776957dc861.

--- CARLOS/Codellege/JavaScript/JS/Pelotita.js
+++ CARLOS/Codellege/JavaScript/JS/Pelotita.js
@@ -1,43 +1,17 @@
 
-var maxX=window.innerWidth;
-var maxY=window.innerHeight;
+var maxX=innerWidth;
+var maxY=innerHeight;
 var minX= 0;
 var minY= 0;    
-
+var pelota= {
+    Movimiento: setInterval(function(){
+    rigth: 50;
+    bottom: 50;
+    },50)
+}
 
 
 $(function()
-{  
-var left= 0;
-var top= 0;
-var pelota= $("#pelota");
-var incY= 1;
-var incX=1;
-pelota.css({top: top, left: left})
-
-setInterval(function(){
-maxX= window.innerWidth;
-maxY= window.innerHeight;
-if(top<=minY){
-incY= 1;
-pelota.css( "background","red");
-}else if(top>= (maxY - pelota.height()))
 {
-incY=-1;
-pelota.css( "background","blue");
-}
-if(left<= minX){
-    incX=1;
-    pelota.css( "background","orange");
-
-}else if(left>= (maxX- pelota.height())){
-incX=-1;
-pelota.css( "background","yellow");
-}
-pelota.css({
-    top: top+= (2 * incY),
-    left: left+=(2 * incX),
-})
-},10);
-
+$("#pelota").on(pelota.Movimiento);
 });
